ec4dcbf7e5d05f96c2b14c26bc66f90118cdba5b
Igor Oliveira <44407469+IgorOliveira42@users.noreply.github.com>
2023-07-21 17:48:04 -0300

    :recycle: refactor: adjustments in main.py file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''https://github.com/IgorOliveira42/StatusPlanilhaCSV'''
 import re
 import glob
 import os
@@ -6,25 +7,41 @@ import pandas as pd
 BASE_DIR = '.'
 files = glob.glob(os.path.join(BASE_DIR, '*.csv'))
 
+
 def verify_signal_from_csv(signal):
+    '''
+    This function is responsible to store the filtered value of signal
+    in float_value var and return a string based on a condition.
+    @param: signal Value of signal column.
+    '''
     try:
         float_value = float(re.findall(r"-?\d+(?:\.\d+)?", signal)[0])
         return "SINAL BOM" if float_value > -29 else "SINAL RUIM"
-    except:
+    except KeyError:
         return None
 
 
 def fill_status_in_file_lines(file):
-    df = pd.read_csv(file, header=1)
-    
-    if 'Status' not in df.columns:
-        df.insert(df.columns.get_loc('Sinal') + 1, 'Status', None)
+    '''
+    This function is responsible to read a csv file, check if column
+    Status is present, create if not, and write Status value at
+    respective column.
+    @param: file CSV File
+    '''
+    read_csv = pd.read_csv(file, header=1)
 
-    df['Status'] = df['Sinal'].apply(verify_signal_from_csv)
-    df.to_csv(file, index=False)
+    if 'Status' not in read_csv.columns:
+        read_csv.insert(read_csv.columns.get_loc('Sinal') + 1, 'Status', None)
+
+    read_csv['Status'] = read_csv['Sinal'].apply(verify_signal_from_csv)
+    read_csv.to_csv(file, index=False)
 
 
 def main():
+    '''
+    This function is responsible to execute the function below inside
+    a for loop that iterate over files .csv present in local directory.
+    '''
     for file in files:
         fill_status_in_file_lines(file)
 
